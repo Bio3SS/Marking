643f79fdee9e62af0694fca361a0c3b74efcc2cd
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-03-13 22:36:34 -0400

    good night (Te)

--- ourscore.R
+++ ourscore.R
@@ -2,4 +2,13 @@ library(dplyr)
 
 library(shellpipes)
 
-tsvRead() %>% summary
+responses <- tsvRead(col_names=FALSE)
+print (responses%>% mutate_if(is.character, as.factor) %>% summary)
+
+key <- csvRead()
+summary(key)
+
+answers <- as.matrix(responses[-(1:2)])
+answers <- matrix(match(answers, LETTERS), nrow=nrow(answers))
+dim(answers)
+summary(answers)
